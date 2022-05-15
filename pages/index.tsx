16d870a9352d8ec040d9f745d1454805b4731518
Stephen Krider <skrider@berkeley.edu>
2022-05-14 18:00:02 -0700

    background done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,56 @@
 import type { NextPage } from "next";
-import { Box } from "@chakra-ui/react";
+import { Box, Input, Text, Select } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
-  return <Box color={"green"}>Hello</Box>;
+  return (
+    <Box
+      bg="background.dark"
+      position="relative"
+      width="100vw"
+      height="100vh"
+      overflow="hidden"
+    >
+      <Box
+        opacity="0.15"
+        position="absolute"
+        top="0"
+        left="0"
+        width="9999px"
+        height="100%"
+        __css={{
+          "& > video": {
+            height: "100%",
+          },
+        }}
+      >
+        <video playsInline autoPlay muted loop>
+          <source src="/terrafinder.mp4" type="video/mp4" />
+        </video>
+      </Box>
+      <Box width="100%" height="100%">
+        <Box width="100%" display="flex" justifyContent="flex-end">
+          <Select placeholder="Select option">
+            <option>Mainnet</option>
+            <option>Testnet</option>
+            <option>Localterra</option>
+          </Select>
+        </Box>
+        <Box
+          height="100%"
+          display="flex"
+          flexDir="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Select zIndex="2" placeholder="Select option">
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </Select>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Home;
---
+++ styles/theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = {
+  colors: {
+    background: {
+      dark: "#092385",
+    },
+  },
+};
+
+export default extendTheme(theme);
